d034b3676f47fc4a1496123696fc53d31dce7096
gaoxam123 <minhtengi1412@gmail.com>
2024-03-22 21:21:38 +0100

    ...

--- Basic/Backend/templating_with_ejs/index.js
+++ Basic/Backend/templating_with_ejs/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const redditData = require('./data.json')
+
+app.use(express.static(path.join(__dirname, '/public')))
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
@@ -9,15 +12,25 @@ app.get('/', (req, res) => {
     res.render('home.ejs')
 })
 
+app.get('/cats', (req, res) => {
+    const cats = ['blue', 'rocket', 'monty', 'stephanie', 'winston']
+    res.render('cats', {cats})
+})
+
 app.get('/r/:subreddit', (req, res) => {
     const {subreddit} = req.params
-    res.render('subreddit.ejs', {subreddit})
+    const data = redditData[subreddit]
+    console.log(data)
+    if(data)
+        res.render('subreddit.ejs', {...data})
+    else 
+        res.render('notfound', {subreddit})
 })
 
 app.get('/rand', (req, res) => {
     const num = Math.floor(Math.random() * 10) + 1
     // can pass variables to the file
-    res.render('random.ejs', {rand: num})
+    res.render('random.ejs', {rand: num, title: 'Random'})
 })
 
 app.listen(3000, () => {
