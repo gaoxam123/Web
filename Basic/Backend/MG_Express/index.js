25e37a7729d912475ecd65ffbc8978eaf2af7776
gaoxam123 <minhtengi1412@gmail.com>
2024-03-24 01:12:01 +0100

    first basic app with mongoose and express

--- Basic/Backend/MG_Express/index.js
+++ Basic/Backend/MG_Express/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 
 const Product = require('./models/product')
 
@@ -17,10 +18,70 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand', { useNewUrlParser: true,
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
+const categories = ['fruit', 'vegetable', 'dairy']
+
+
+// SHOW EVERYTHING
 app.get('/products', async(req, res) => {
-    const products = await Product.find({})
-    console.log(products)
-    res.render('products/index', {products})
+    // extract query string
+    const {category} = req.query
+    if(!category) {
+        const products = await Product.find({})
+        res.render('products/index', {products, allproduct: true})
+    }
+    else {
+        const products = await Product.find({category: category})
+        res.render('products/index', {products, allproduct: false, category})
+    }
+    // console.log(products)
+})
+
+
+// PROCEED TO NEW
+app.get('/products/new', (req, res) => {
+    res.render('products/new', {categories})
+})
+
+// ADD NEW ONE
+app.post('/products', async(req, res) => {
+    const newProduct = new Product(req.body)
+    await newProduct.save()
+    res.redirect(`/products/${newProduct._id}`)
+})
+
+// SHOW ONE PRODUCT
+// req.params used in app.get, taken from the string
+// req.body used when some info were sent to the link, e.g submit a form
+app.get('/products/:id', async(req, res) => {
+    const {id} = req.params
+    const product = await Product.findById(id)
+    // console.log(product)
+    res.render('products/show', {product})
+})
+
+
+// PROCEED TO EDIT
+app.get('/products/:id/edit', async(req, res) => {
+    const {id} = req.params
+    const product = await Product.findById(id)
+    res.render('products/edit', {product, categories})
+})
+
+// EDIT ONE PRODUCT
+app.put('/products/:id', async(req, res) => {
+    const {id} = req.params
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true})
+    res.redirect(`/products/${id}`)
+})
+
+// DELETE BY ID
+app.delete('/products/:id', async(req, res) => {
+    const {id} = req.params
+    await Product.findByIdAndDelete(id)
+    res.redirect('/products')
 })
 
 app.listen(3000, () => {
