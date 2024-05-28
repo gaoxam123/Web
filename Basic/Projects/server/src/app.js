83c6428ec08f2657d2459778ae95744d9b728a39
gaoxam123 <minhtengi1412@gmail.com>
2024-05-28 21:02:25 +0200

    add public, layouts and partials folders

--- Basic/Projects/server/src/app.js
+++ Basic/Projects/server/src/app.js
@@ -21,6 +21,7 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(methodOverride('_method'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/recipes', recipeRouter)
 
